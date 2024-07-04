5ec59aae3393ab66448b236ebfd29e92ee89fb41
dldusgh318 <pololydotoly@gmail.com>
2024-07-05 05:13:12 +0900

    feat1:둥

--- src/main/java/umc_haekathon_4/demo/web/controller/MemoryController.java
+++ src/main/java/umc_haekathon_4/demo/web/controller/MemoryController.java
@@ -1,5 +1,6 @@
 package umc_haekathon_4.demo.web.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -25,6 +26,7 @@ public class MemoryController {
 
     //추억 등록
     @PostMapping("/memory/upload")
+    @Operation(summary = "추억 등록", description = "새로운 추억을 등록합니다.")
     public ApiResponse<MemoryResponseDTO> create(@RequestBody @Valid MemoryRequestDTO.CreateMemoryDto request) {
         Memory memory = memoryService.createMemory(request);
         return ApiResponse.onSuccess(MemoryConverter.convertToDto(memory));
@@ -38,6 +40,7 @@ public class MemoryController {
 
     //추억 조회
     @GetMapping("/memory")
+    @Operation(summary = "추억 조회", description = "주어진 ID의 추억을 조회합니다.")
     public ApiResponse<Memory> getMemory(Long memoryId) {
         Memory memory = memoryService.getMemory(memoryId);
         return ApiResponse.onSuccess(memory);
@@ -46,6 +49,7 @@ public class MemoryController {
 
     //모든 추억 미리보기
     @GetMapping("/memory-preview")
+    @Operation(summary = "모든 추억 미리보기", description = "등록된 모든 추억을 미리 봅니다.")
     public ApiResponse<List<Memory>> getAllMemories(){
         List<Memory> memories = memoryService.getAllMemories();
         return ApiResponse.onSuccess(memories);
