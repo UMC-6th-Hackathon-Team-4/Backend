95fc5fd7cd7bcb81b22b2993e589a9a4d1129519
jiyoung <gyjeong021@gmail.com>
2024-07-04 22:16:57 +0900

    feat : 보물상자, 사용자 도메인 구현

---
+++ src/main/java/umc_haekathon_4/demo/domain/TeasureBox.java
@@ -0,0 +1,27 @@
+package umc_haekathon_4.demo.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class TeasureBox {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime deadline;
+    private String status;
+    private String title;
+    private String location;
+}
